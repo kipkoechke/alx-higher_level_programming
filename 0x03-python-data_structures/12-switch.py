15c52ad2d4c31d7ee75229d04196668d69d834b1
kipkoechke <chepkwonyke1@gmail.com>
2023-08-31 20:14:17 +0000

    Fixed the number of lines

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-# Swapping values using tuple assignment
-
-# Initial values
 a = 89
 b = 10
-
-# Swapping using tuple assignment
 a, b = b, a
-
-# Displaying the swapped values
 print("After swapping: a={:d} - b={:d}".format(a, b))
