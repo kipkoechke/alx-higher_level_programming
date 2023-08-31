de27d77f1ecb625c06181b357c25e073e57dd8d4
kipkoechke <chepkwonyke1@gmail.com>
2023-08-31 19:23:15 +0000

    Fixed Pycodestyle

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -13,7 +13,8 @@ def replace_in_list(my_list, idx, element):
         list: The modified list with the replacement.
 
     Note:
-        This function replaces the element at the given index with the new element.
+        This function replaces the element at the given index
+          with the new element.
         If the index is out of bounds, the original list remains unchanged.
     """
     if idx >= 0 and idx < len(my_list):
