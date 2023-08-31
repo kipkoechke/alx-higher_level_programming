49da34f7372026f086044138b285d757827dc534
kipkoechke <chepkwonyke1@gmail.com>
2023-08-31 20:08:45 +0000

    Switch

---
+++ 0x03-python-data_structures/12-switch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Swapping values using tuple assignment
+
+# Initial values
+a = 89
+b = 10
+
+# Swapping using tuple assignment
+a, b = b, a
+
+# Displaying the swapped values
+print("After swapping: a={:d} - b={:d}".format(a, b))
