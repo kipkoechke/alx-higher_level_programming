c82026cd9d7b2b037341917d84d51dc5af138d35
kipkoechke <chepkwonyke1@gmail.com>
2023-09-12 19:17:33 +0000

    Fixed - Pycodestyle validation

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This script defines a function for appending text to a file."""
 
+
 def append_write(filename="", text=""):
     """
     Appends a string to the end of a UTF8 text file.
