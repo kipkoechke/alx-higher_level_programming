5bf1454e39580b14bf85f311d9f24fe6e983fe25
kipkoechke <chepkwonyke1@gmail.com>
2023-09-13 18:44:25 +0000

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,19 +1,20 @@
-tats.py
-"""Reads from standard input and calculates metrics.
+#!/usr/bin/python3
+# 101-stats.py
+"""Reads from standard input and computes metrics.
 
-After every ten lines or upon a keyboard interruption (CTRL + C),
-it prints the following statistics:
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
 
 
 def print_stats(size, status_codes):
-    """Prints accumulated metrics.
+    """Print accumulated metrics.
 
     Args:
-        size (int): The total accumulated file size.
-        status_codes (dict): The count of accumulated status codes.
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
