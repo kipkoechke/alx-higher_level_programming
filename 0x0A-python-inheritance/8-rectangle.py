80b990d7443bdfcebe98a4d11155403890c6a6cb
kipkoechke <chepkwonyke1@gmail.com>
2023-09-14 19:14:58 +0000

    Fixed - Pycodestyle validation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,6 +5,7 @@ This module defines a Rectangle class that inherits from BaseGeometry.
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     A class to define a rectangle using BaseGeometry.
