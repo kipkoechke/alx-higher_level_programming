7f88d0d6319680fae3493648e95ef69d9171ff61
kipkoechke <chepkwonyke1@gmail.com>
2023-09-15 20:27:20 +0000

    Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,18 @@ def area(self):
     """Calculate and return the area of the Rectangle."""
     return self.width * self.height
 
+def display(self):
+    """Print the Rectangle using the `#` character.
+
+    If the width or height is 0, no characters are printed.
+    """
+    if self.width == 0 or self.height == 0:
+        print("")
+        return
+
+    [print("") for y in range(self.y)]
+    for h in range(self.height):
+        [print(" ", end="") for x in range(self.x)]
+        [print("#", end="") for w in range(self.width)]
+        print("")
+
