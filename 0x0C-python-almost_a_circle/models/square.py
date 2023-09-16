8482bac328c8a2cc9bebc266df7d82058eb921ad
kipkoechke <chepkwonyke1@gmail.com>
2023-09-16 17:16:33 +0000

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# square.py
+"""Defines a square class."""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """Represents a square.
+
+    Attributes:
+        size (int): The size of the square.
+        x (int): The x-coordinate of the square.
+        y (int): The y-coordinate of the square.
+        id (int): The identity of the square.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+        super().__init__(size, size, x, y, id)
+
+
