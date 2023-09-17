a9f0cc940bcf070e9caa41d15e169d9d23d6ff7d
kipkoechke <chepkwonyke1@gmail.com>
2023-09-17 17:49:31 +0000

    Fixed - indentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,87 +80,87 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-def area(self):
-    """Calculate and return the area of the Rectangle."""
-    return self.width * self.height
+    def area(self):
+        """Calculate and return the area of the Rectangle."""
+        return self.width * self.height
 
-def display(self):
-    """Print the Rectangle using the `#` character.
+    def display(self):
+        """Print the Rectangle using the `#` character.
 
-    If the width or height is 0, no characters are printed.
-    """
-    if self.width == 0 or self.height == 0:
-        print("")
-        return
+        If the width or height is 0, no characters are printed.
+        """
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
 
-    [print("") for y in range(self.y)]
-    for h in range(self.height):
-        [print(" ", end="") for x in range(self.x)]
-        [print("#", end="") for w in range(self.width)]
-        print("")
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
 
-def update(self, *args, **kwargs):
-    """Update the Rectangle attributes.
+    def update(self, *args, **kwargs):
+        """Update the Rectangle attributes.
 
-    Args:
-        *args (ints): New attribute values.
-            - 1st argument represents the id attribute
-            - 2nd argument represents the width attribute
-            - 3rd argument represents the height attribute
-            - 4th argument represents the x attribute
-            - 5th argument represents the y attribute
-        **kwargs (dict): New key/value pairs of attributes.
-    """
-    if args and len(args) != 0:
-        a = 0
-        for arg in args:
-            if a == 0:
-                if arg is None:
-                    self.__init__(self.width, self.height, self.x, self.y)
-                else:
-                    self.id = arg
-            elif a == 1:
-                self.width = arg
-            elif a == 2:
-                self.height = arg
-            elif a == 3:
-                self.x = arg
-            elif a == 4:
-                self.y = arg
-            a += 1
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents the id attribute
+                - 2nd argument represents the width attribute
+                - 3rd argument represents the height attribute
+                - 4th argument represents the x attribute
+                - 5th argument represents the y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
 
-    elif kwargs and len(kwargs) != 0:
-        for k, v in kwargs.items():
-            if k == "id":
-                if v is None:
-                    self.__init__(self.width, self.height, self.x, self.y)
-                else:
-                    self.id = v
-            elif k == "width":
-                self.width = v
-            elif k == "height":
-                self.height = v
-            elif k == "x":
-                self.x = v
-            elif k == "y":
-                self.y = v
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
-def to_dictionary(self):
-    """Convert the Rectangle attributes to a dictionary."""
-    return {
-        "id": self.id,
-        "width": self.width,
-        "height": self.height,
-        "x": self.x,
-        "y": self.y
-    }
+    def to_dictionary(self):
+        """Convert the Rectangle attributes to a dictionary."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
 
-def __str__(self):
-    """Return the string representation of the Rectangle.
+    def __str__(self):
+        """Return the string representation of the Rectangle.
 
-    Returns:
-        str: A formatted string representing the Rectangle.
-    """
-    return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                   self.x, self.y,
-                                                   self.width, self.height)
+        Returns:
+            str: A formatted string representing the Rectangle.
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
