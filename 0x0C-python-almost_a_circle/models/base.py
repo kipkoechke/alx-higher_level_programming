c9ada3058ad0062fed85e78cf41449e23287f66e
kipkoechke <chepkwonyke1@gmail.com>
2023-09-17 16:18:54 +0000

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,6 @@
 # base.py
 """Defines a base model class."""
 
-
 class Base:
     """Represents the base model.
 
@@ -25,3 +24,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Serialize a list of dictionaries to JSON.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+            
+        Returns:
+            str: A JSON string representation of the list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
