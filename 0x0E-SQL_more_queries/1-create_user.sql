5bfd7b534a297d0b6205c8314f45f6e47d1a4d28
kipkoechke <chepkwonyke1@gmail.com>
2023-10-18 20:49:12 +0000

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,13 +1,11 @@
--- Create a new database user 'user_0d_1' at the 'localhost' host and set the password.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
+-- Create the database user 'user_0d_1' at the 'localhost' host and set the password.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges on all databases and tables to 'user_0d_1' at the 'localhost' host.
-GRANT ALL PRIVILEGES
-    ON *.*
+GRANT ALL PRIVILEGES ON *.*
     TO 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 
--- Ensure that the privileges are updated to apply the changes immediately.
+-- Ensure that the privileges are refreshed to apply the changes immediately.
 FLUSH PRIVILEGES;
