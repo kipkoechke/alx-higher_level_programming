b752f902d66c5aec16d49e500ceff1b3e5e36314
kipkoechke <chepkwonyke1@gmail.com>
2023-10-18 20:52:19 +0000

    Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Retrieve a list of TV shows in 'hbtn_0d_tvshows' with at least one linked genre.
+-- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+INNER JOIN `tv_show_genres` AS g
+ON s.`id` = g.`show_id`
+ORDER BY s.`title`, g.`genre_id`;
