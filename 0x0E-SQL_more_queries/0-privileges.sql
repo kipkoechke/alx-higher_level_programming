ea9de23584f741d596a5e5c675701b52005baa36
kipkoechke <chepkwonyke1@gmail.com>
2023-10-18 20:10:38 +0000

    My privileges!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Display the privileges for the user 'user_0d_1' at the 'localhost' host.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show the permissions for the user 'user_0d_2' at the 'localhost' host.
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
