ead317a57f91ecfabe9e997bb134841282d873b9
kipkoechke <chepkwonyke1@gmail.com>
2023-10-18 20:18:58 +0000

    Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,13 @@
+-- Create a new database user 'user_0d_1' at the 'localhost' host and set the password.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases and tables to 'user_0d_1' at the 'localhost' host.
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Ensure that the privileges are updated to apply the changes immediately.
+FLUSH PRIVILEGES;
