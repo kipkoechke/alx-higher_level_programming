f5ff259de9e9a8b7268c6611b728b4021edf4547
kipkoechke <chepkwonyke1@gmail.com>
2023-10-19 16:31:25 +0000

    Temperatures #1

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Retrieve the three cities with the highest average temperatures
+-- for the months of July and August.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
