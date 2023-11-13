57b2f2ae0e65057c370ce1960c1959f0e8d529c8
kipkoechke <chepkwonyke1@gmail.com>
2023-11-14 00:02:33 +0300

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 let numberOfTimes = parseInt(process.argv[2]);
 if (!isNaN(numberOfTimes)) {
   while (numberOfTimes > 0) {
-    console.log('C is fun'); 
+    console.log('C is fun');
     numberOfTimes -= 1;
   }
 } else {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 }
