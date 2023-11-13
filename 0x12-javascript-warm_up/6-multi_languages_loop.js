fed52e1d7dc55079cd3bd819c511715ad6f6a91f
kipkoechke <chepkwonyke1@gmail.com>
2023-11-13 23:35:58 +0300

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const complements = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i = 0;
+while (i < complements.length) {
+  console.log(complements[i]);
+  i++;
+}
