53d4f012f816ba92aa6d600d8675d34f42c4560c
kipkoechke <chepkwonyke1@gmail.com>
2023-11-13 20:52:32 +0000

    Fix -  Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const complements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let complement of complements) {
+for (const complement of complements) {
   console.log(complement);
 }
