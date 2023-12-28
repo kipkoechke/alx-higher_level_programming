9c9bd0f09a6cd4c821b0e0a503a0283b477c16df
kipkoechke <chepkwonyke1@gmail.com>
2023-12-28 10:18:20 +0300

    Refactored code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def fizzbuzz():
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
@@ -9,4 +10,4 @@ def fizzbuzz():
         elif number % 5 == 0:
             print("Buzz ", end="")
         else:
-            print("{} ".format(number), end="")
+            print(f"{number} ", end="")
