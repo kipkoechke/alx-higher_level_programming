3e3653677273323fc7ff13d06c65341d0a0c95f6
kipkoechke <chepkwonyke1@gmail.com>
2023-12-30 11:00:37 +0300

    Return a new dictionary with all values multiplied by 2

---
+++ 9-multiply_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# 9-multiply_by_2.py
+
+
+def multiply_by_2(a_dictionary):
+    """
+    Return a new dictionary with all values multiplied by 2.
+
+    Args:
+        a_dictionary (dict): The dictionary to process.
+
+    Returns:
+        dict: A new dictionary with all values multiplied by 2.
+    """
+    return {key: value * 2 for key, value in a_dictionary.items()}
