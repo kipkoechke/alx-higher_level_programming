7d3950b70da338dd0b5b437727be55e0a89532cf
kipkoechke <chepkwonyke1@gmail.com>
2023-12-30 10:45:36 +0300

    Replace or add a key/value pair in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 # 7-update_dictionary.py
 
+
 def update_dictionary(a_dictionary, key, value):
     """
-    Replace or add key/value pairs in a dictionary.
+    Replace or add a key/value pair in a dictionary.
 
     Args:
-        a_dictionary (dict): The input dictionary.
-        key: The key to replace or add.
+        a_dictionary (dict): The dictionary to update.
+        key (str): The key to add or replace.
         value: The value to associate with the key.
 
     Returns:
-        dict: The updated dictionary with the key/value pair.
+        dict: The updated dictionary.
     """
     a_dictionary[key] = value
-    return (a_dictionary)
+    return a_dictionary
