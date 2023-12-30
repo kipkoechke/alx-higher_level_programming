f3199220a8dc8fce5eaefc221a009d24aebbc036
kipkoechke <chepkwonyke1@gmail.com>
2023-12-30 11:10:36 +0300

    Modified code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -13,3 +13,10 @@ def multiply_by_2(a_dictionary):
         dict: A new dictionary with all values multiplied by 2.
     """
     return {k: a_dictionary[k] * 2 for k in a_dictionary}
+
+
+a_dictionary = {"John": 12, "Alex": 8, "Bob": 14, "Mike": 14, "Molly": 16}
+print_sorted_dictionary = __import__(
+    "6-print_sorted_dictionary"
+).print_sorted_dictionary
+print_sorted_dictionary(a_dictionary)
