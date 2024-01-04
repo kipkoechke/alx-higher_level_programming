e9e6473a448cb8ae842cd6f896e6e5c153db93c1
kipkoechke <chepkwonyke1@gmail.com>
2024-01-04 03:34:05 +0300

    Fix - failing checks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,27 @@
-#!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
+request = require("request");
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+function printCharacters(characters, index) {
+  if (index < characters.length) {
+    request(
+      { url: characters[index], json: true },
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body.name);
+          printCharacters(characters, index + 1);
+        }
+      }
+    );
+  }
+}
+
+function main() {
+  const movieId = process.argv[2];
+  request(
+    { url: `https://swapi.dev/api/films/${movieId}/`, json: true },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        printCharacters(body.characters, 0);
       }
     }
-  });
+  );
 }
