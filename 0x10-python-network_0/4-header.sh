ee05f9912646d7ca2e0fa84db0dd6ee4a4798904
kipkoechke <chepkwonyke1@gmail.com>
2024-01-07 21:22:36 +0300

    Send a GET request to the arg specified with a custom header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Send a GET request to the arg specified with a custom header
+curl -s -X GET --header "X-School-User-Id: 98" "$1"
